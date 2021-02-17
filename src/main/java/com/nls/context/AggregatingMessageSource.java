2037e2feaddafc15a6e7e30aafe0d272d29dce71
Marc <marc.g.smith@gmail.com>
2021-02-17 19:41:18 +0000

    Ignore default on resolvable until all sources checked

--- src/main/java/com/nls/context/AggregatingMessageSource.java
+++ src/main/java/com/nls/context/AggregatingMessageSource.java
@@ -12,15 +12,22 @@ public class AggregatingMessageSource implements MessageSource {
     private List<MessageSource> messageSources = new ArrayList<>(1);
 
     public String getMessage(MessageSourceResolvable resolvable, Locale locale) throws NoSuchMessageException {
+        MessageSourceResolvable nodefault = new NoDefaultMessageSourceResolvable(resolvable);
         for (MessageSource source : messageSources) {
             try {
-                String result = source.getMessage(resolvable, locale);
+                String result = source.getMessage(nodefault, locale);
                 if (result != null) {
                     return result;
                 }
             } catch (NoSuchMessageException ignore) {
             }
         }
+
+        String result = messageSources.get(0).getMessage(resolvable, locale);
+        if (result != null) {
+            return result;
+        }
+
         String[] codes = resolvable.getCodes();
         throw new NoSuchMessageException(codes.length > 0 ? codes[codes.length - 1] : null, locale);
     }
@@ -51,4 +58,27 @@ public class AggregatingMessageSource implements MessageSource {
         this.messageSources = messageSources;
     }
 
+
+    private class NoDefaultMessageSourceResolvable implements MessageSourceResolvable {
+        private final MessageSourceResolvable delegate;
+
+        private NoDefaultMessageSourceResolvable(MessageSourceResolvable delegate) {
+            this.delegate = delegate;
+        }
+
+        @Override
+        public String[] getCodes() {
+            return delegate.getCodes();
+        }
+
+        @Override
+        public Object[] getArguments() {
+            return delegate.getArguments();
+        }
+
+        @Override
+        public String getDefaultMessage() {
+            return null;
+        }
+    }
 }
